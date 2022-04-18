954342491f2881d5e3fe034d2fc8a73c3110f5c0
Ilmari Vainio <ilmari.vainio01@gmail.com>
2022-04-18 14:20:21 +0300

    Revert "Hope this does not cause issues"
    This reverts commit a3720dc0383602d8f106c981082ddacdf2cd75f0.

--- My project/Assets/Scripts/Character/CharacterStats.cs
+++ My project/Assets/Scripts/Character/CharacterStats.cs
@@ -6,29 +6,11 @@ namespace FrogTime
 {
     internal class CharacterStats : CharacterVariables
     {
-        [SerializeField]
-        GameObject death_Message;
-        [SerializeField]
-        CharacterMovement characterMovement_script;
-        [SerializeField]
-        CharacterAttack characterAttack_Script;
-
-
         private void Awake()
         {
+            
             characterHealt = 20;
             characterDamage = 2;
-        }
-
-
-        private void Update()
-        {
-            if (characterHealt < 0)
-            {
-                death_Message.SetActive(true);
-                characterAttack_Script.enabled = false;
-                characterMovement_script.enabled = false;
-            }
         }
     } 
 }
