44ec277c77e73c8d30ebd31f27866d0bdf765780
polari0 <63444628+polari0@users.noreply.github.com>
2022-04-20 09:35:00 +0300

    Things

--- My project/Assets/Scripts/Generic/Variables.cs
+++ My project/Assets/Scripts/Generic/Variables.cs
@@ -7,8 +7,8 @@ namespace FrogTime
     internal class CharacterVariables : MonoBehaviour
     {
         //Character Variables 
-        internal int characterMaxHealt = 20;
-       internal int characterHealt = 20;
+       internal float characterMaxHealt = 20f;
+       internal float characterHealt = 20f;
        internal float characterSpeed;
        internal int characterDamage = 2;
        internal static float characterMoveSpeed = 0.1f;
